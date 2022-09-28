3fb8e045a9a620fe2815c8f1cf9129cc866f0189
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-09-28 10:15:39 +0600

    project maneger aplication server intial commit

--- server.js
+++ server.js
@@ -12,29 +12,29 @@ global.io = io;
 const router = jsonServer.router("db.json");
 
 // response middleware
-router.render = (req, res) => {
-  const path = req.path;
-  const method = req.method;
+// router.render = (req, res) => {
+//   const path = req.path;
+//   const method = req.method;
 
-  if (
-    path.includes("/conversations") &&
-    (method === "POST" || method === "PATCH")
-  ) {
-    // emit socket event
-    io.emit("conversation", {
-      data: res.locals.data,
-    });
-  }
+//   if (
+//     path.includes("/conversations") &&
+//     (method === "POST" || method === "PATCH")
+//   ) {
+//     // emit socket event
+//     io.emit("conversation", {
+//       data: res.locals.data,
+//     });
+//   }
 
-  if (path.includes("/messages") && method === "POST") {
-    // emit socket event
-    io.emit("message", {
-      data: res.locals.data,
-    });
-  }
+//   if (path.includes("/messages") && method === "POST") {
+//     // emit socket event
+//     io.emit("message", {
+//       data: res.locals.data,
+//     });
+//   }
 
-  res.json(res.locals.data);
-};
+//   res.json(res.locals.data);
+// };
 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
@@ -46,8 +46,8 @@ app.use(middlewares);
 
 const rules = auth.rewriter({
   users: 640,
-  conversations: 660,
-  messages: 660,
+  // conversations: 660,
+  // messages: 660,
 });
 
 app.use(rules);
